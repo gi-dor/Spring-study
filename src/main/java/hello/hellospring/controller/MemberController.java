e8009adce92394c29c345a0c9e99f03e22e44055
gi-dor <rltjs3563@naver.com>
2024-03-04 22:08:25 +0900

    Comment :  @Controller

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
-
+// Spring 컨테이너에 @Controller 가 있으면  MemberController 객체를 생성해 스프링 빈 객체로 관리한다
 @Controller
 public class MemberController {
 
