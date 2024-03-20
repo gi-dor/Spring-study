cf82e617fc47d081e4399bdd04d4b1e7f94f2a86
gi-dor <rltjs3563@naver.com>
2024-03-20 20:24:28 +0900

    Feat : MemberList

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,9 +2,11 @@ package hello.hellospring.controller;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -34,8 +36,17 @@ public class MemberController {
         Member member = new Member();
         member.setName(form.getName());
 
+        System.out.println("member = "+member);
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members =  memberService.findMembers();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
     }
 }
