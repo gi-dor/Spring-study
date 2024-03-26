aa49952c2eba6c8f0b9b3b20ece206fd08c08f9e
gi-dor <rltjs3563@naver.com>
2024-03-26 15:55:24 +0900

    Style : @Entity 추가 , @Column 삭제

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,20 +1,19 @@
 package hello.hellospring.domain;
 
-
-import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Member {
 
     @Id // 해당 프로퍼티가 엔티티를 식별하는 값을 포함 , 기본 키 컬럼의 값을 표현함
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 값을 생성하느 방법을 지정 , IDENTITY - 기본키 생성을 DB에 위임  DB가 자동으로 auto_incresement 를 사용해 기본키 값 생성
     private Long id;
 
-   @Column(name = "username")   // DB 컬럼명이 "username" 과 매핑했음
+//   @Column(name = "username")   // DB 컬럼명이 "username" 과 매핑했음
     private String name;
-
 
 
     public Long getId() {
